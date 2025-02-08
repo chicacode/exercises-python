9b80bcedd2912f0261358122530a529c599bbac3
chicacode <gerahernan.al@gmail.com>
2025-02-07 17:56:44 -0800

    Adding list loops operations and functions files

---
+++ numpy_exercises/intro_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+weight = [70.2, 50.7, 46.9, 80.3, 67.8]
+height = [1.69, 1.70, 1.40, 1.80, 1.50]
+
+np_weight = np.array(weight)
+np_height = np.array(height)
+
+print('result ', np_weight / np_height ** 2)
